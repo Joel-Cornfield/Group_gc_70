5f8df8e33e46c33181fa48c5da9ea8075ffed0e6
FluffyLiam <malachymcgrath286@gmail.com>
2025-05-01 11:21:38 +0800

    Filled form file out

--- app/forms.py
+++ app/forms.py
@@ -1,3 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
+
+### Login Form ###
+class LoginForm(FlaskForm):
+
+### Registration Form ###
+class RegistrationForm(FlaskForm):
+
+### Location Form ###
+class LocationForm(FlaskForm):
+
+### Change Password Form ###
+class ChangePasswordForm(FlaskForm):

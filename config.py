5b089032f4f06eb911f7bad739aed0870d256477
FluffyLiam <malachymcgrath286@gmail.com>
2025-06-03 16:35:01 +0800

    Changed Pooling for SQLalchemy

--- config.py
+++ config.py
@@ -1,10 +1,16 @@
+from sqlalchemy.pool import NullPool
 import os 
 basedir = os.path.abspath(os.path.dirname(__file__))
+
+
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        "poolclass": NullPool
+    }
 
 class TestConfig:
     TESTING = True

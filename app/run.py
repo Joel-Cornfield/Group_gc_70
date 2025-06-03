8305e4c810f8bd57b349aa560882d64c4d573366
FluffyLiam <malachymcgrath286@gmail.com>
2025-06-03 16:24:53 +0800

    Changed requirements for deployment

--- app/run.py
+++ app/run.py
@@ -1,7 +1,10 @@
 from app import create_app, socketio, db
+from flask_migrate import Migrate
 
 app = create_app()
+migrate = Migrate(app, db)
+
 
 if __name__ == "__main__":
     socketio.run(app)
-    migrate = Migrate(app, db)
+    
